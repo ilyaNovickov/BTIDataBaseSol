29e66be71261bd83389164a9c0be03a34330fd40
Ilya Novikov <ilyanovickov@mail.ru>
2024-04-12 18:37:12 +0300

    .

--- BTIDataBaseProj/MainWindow.xaml.cs
+++ BTIDataBaseProj/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,47 @@ namespace BTIDataBaseProj
     /// </summary>
     public partial class MainWindow : Window
     {
+        BTIDataBaseEntities contex = new BTIDataBaseEntities();
+        CollectionViewSource buildingsViewSourse;
+        CollectionViewSource flatsViewSourse;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            buildingsViewSourse = ((CollectionViewSource)(FindResource("buildingsTableViewSource")));
+            flatsViewSourse = ((CollectionViewSource)(FindResource("buildingsTableFlatsTableViewSource")));
+
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Data.CollectionViewSource buildingsTableViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("buildingsTableViewSource")));
+            // Загрузите данные, установив свойство CollectionViewSource.Source:
+            // buildingsTableViewSource.Source = [универсальный источник данных]
+
+            contex.BuildingsTable.Load();
+
+            buildingsViewSourse.Source = null;//contex.BuildingsTable.Local;
+
+            contex.FlatsTable.Load();
+
+            flatsViewSourse.Source = contex.FlatsTable.Local;
+
+            //buildingsTableFlatsTableViewSource
+        }
+
+        private void buildingsTableDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            buildingsViewSourse.View?.Refresh();
+
+
+
+            if (flatsTableDataGrid.SelectedItem is FlatsTable flat)
+            {
+                buildingsViewSourse.Source = from b in contex.BuildingsTable.ToList() where b.Kadastr == flat.BuildingKadastr select b;
+                    }
         }
     }
 }
